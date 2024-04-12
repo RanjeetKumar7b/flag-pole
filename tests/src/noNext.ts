09ad2412501afdee48c58c6e967d74af3d52ddd4
Ranjeet Kumar <77196517+RanjeetKumar7b@users.noreply.github.com>
2024-04-12 08:32:16 +0530

    Delete tests directory

--- tests/src/noNext.ts
+++
@@ -1,7 +0,0 @@
-import flagpole from "../../dist";
-
-const suite = flagpole("No .next will dangle");
-
-suite.maxSuiteDuration = 3000;
-
-suite.scenario("Homepage Loads", "html").open("https://dribbble.com/");
